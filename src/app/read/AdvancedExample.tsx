7b7bd6ac325fbc4b555f5e00f6f6e378d66ae15d
wonsama <imtough99@gmail.com>
2024-03-21 23:13:48 +0900

    temp

--- src/app/read/AdvancedExample.tsx
+++ src/app/read/AdvancedExample.tsx
@@ -69,6 +69,7 @@ export const AdvancedExample: React.FC<IAdvancedExampleProps> = ({
         cameraId={selectedCameraId || cameraDevices[0].id}
       />
       <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         onClick={() => {
           if (ref.current) {
             ref.current.pause();
@@ -78,6 +79,7 @@ export const AdvancedExample: React.FC<IAdvancedExampleProps> = ({
         Pause
       </button>
       <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         onClick={() => {
           if (ref.current) {
             ref.current.resume();
--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -5,7 +5,6 @@ import { useCallback, useState } from "react";
 import { AdvancedExample } from "./AdvancedExample";
 import Link from "next/link";
 import { ScannerExample } from "./ScannerExample";
-import dynamic from "next/dynamic";
 
 enum Example {
   Scanner = "scanner",
@@ -32,7 +31,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="place-items-center before:w-full">
         {selectedExample === Example.Scanner && (
           <ScannerExample onCodeScanned={handleCodeScanned} />
         )}
